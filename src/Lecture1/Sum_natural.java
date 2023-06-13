b9bc78b67d28832a78aeb8a26f5f35ae4232f28c
Shubham <shubham.shubham.singh13@gmail.com>
2023-06-13 13:20:42 +0530

    multiplication table

--- src/Lecture1/Sum_natural.java
+++ src/Lecture1/Sum_natural.java
@@ -3,15 +3,15 @@ package Lecture1;
 import java.util.Scanner;
 
 public class Sum_natural {
-public static void main(String[] args) {
-	Scanner s=new Scanner(System.in);
-	int n= s.nextInt();
-	int i=0;
-	int sum=0;
-	while(i<=n) {
-		sum+=i;
-		i++;
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n= s.nextInt();
+		int i=0;
+		int sum=0;
+		while(i<=n) {
+			sum+=i;
+			i++;
+		}
+		System.out.println(sum);
 	}
-	System.out.println(sum);
-}
 }
---
+++ src/Lecture1/Table.java
@@ -0,0 +1,15 @@
+package Lecture1;
+
+import java.util.Scanner;
+
+public class Table {
+	public static void main(String[] args) {
+		 Scanner s=new Scanner(System.in);
+		 int n=s.nextInt();
+		 int i=1;
+		 while(i<=10) {
+			 System.out.println(n*i);
+			 i++;
+		 }
+	}
+}
