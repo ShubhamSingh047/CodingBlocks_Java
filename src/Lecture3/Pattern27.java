533555840583e52a8297d99eed519600687233ee
Shubham <shubham.shubham.singh13@gmail.com>
2023-06-23 16:43:53 +0530

    basic pattern numbers

--- src/Lecture3/Pattern27.java
+++ src/Lecture3/Pattern27.java
@@ -2,13 +2,12 @@ package Lecture3;
 
 import java.util.Scanner;
 
-public class Pattern25 {
+public class Pattern27 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt();
 		int row=1;
 		int star=1;
-		int sum=1;
 		int space=n-1;
 		while(row<=n) {
 			int i=1;
@@ -18,14 +17,12 @@ public class Pattern25 {
 			}
 			int j=1;
 			while(j<=star) {
-				System.out.print(sum+"\t");
-				sum++;
+				System.out.print(j+"\t");
 				j++;
 			}
-			star+=2;
-			
-			space--;
 			row++;
+			space--;
+			star+=2;
 			System.out.println();
 		}
 	}
