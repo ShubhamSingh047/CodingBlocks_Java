195f5d5ba45f17687638156c5fe5c4d02f2139e2
Shubham <shubham.shubham.singh13@gmail.com>
2023-06-23 16:32:49 +0530

    basic pattern

---
+++ src/Lecture3/Pattern25.java
@@ -0,0 +1,32 @@
+package Lecture3;
+
+import java.util.Scanner;
+
+public class Pattern25 {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int row=1;
+		int star=1;
+		int sum=1;
+		int space=n-1;
+		while(row<=n) {
+			int i=1;
+			while(i<=space) {
+				System.out.print("  ");
+				i++;
+			}
+			int j=1;
+			while(j<=star) {
+				System.out.print(sum+" ");
+				sum++;
+				j++;
+			}
+			star+=2;
+			
+			space--;
+			row++;
+			System.out.println();
+		}
+	}
+}
