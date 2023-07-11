3ad40c5f10872c9cf868d8d8fd09e6c8d9caaca3
Shubham <shubham.shubham.singh13@gmail.com>
2023-07-11 20:37:15 +0530

    subarray sum optimum

---
+++ src/Self/MaximumConsucativeOnce.java
@@ -0,0 +1,18 @@
+package Self;
+
+public class MaximumConsucativeOnce {
+	public static void main(String[] args) {
+		int[] arr= {0,1,1,0,1,0};
+		int res=0;
+		int count=1;
+		for(int i=0; i<arr.length-1; i++) {
+			if(arr[i]==arr[i+1]&& arr[i]==1) {
+				count++;
+				res=Math.max(count, res);
+			}else {
+				count=1;
+			}
+		}
+		System.out.println(res);
+	}
+}
---
+++ src/Self/MaximumSubArrayOptimum.java
@@ -0,0 +1,14 @@
+package Self;
+
+public class MaximumSubArrayOptimum {
+public static void main(String[] args) {
+	int[] arr= {0,1,1,0,1,0};
+	int res=arr[0];
+	int maxEnd=arr[0];
+	for(int i=1; i<arr.length; i++) {
+		maxEnd=Math.max(maxEnd+arr[i], arr[i]);
+		res=Math.max(maxEnd,arr[i]);
+	}
+	System.out.println(res);
+}
+}
