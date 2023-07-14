4dde4a5fe8e5661f8ef81ae02e41d6c1fe3072ea
Shubham <shubham.shubham.singh13@gmail.com>
2023-07-14 13:03:41 +0530

    print substring if palindrom

--- src/Lecture12/PalindromCheck.java
+++ src/Lecture12/PalindromCheck.java
@@ -6,16 +6,18 @@ public class PalindromCheck {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		String s1=s.nextLine();
+		System.out.println(Palindrom(s1));
 		
+	}
+	public static boolean Palindrom(String s1) {
 		int i=0, j=s1.length()-1;
 		while(i<=j) {
 			if(s1.charAt(i)!=s1.charAt(j)) {
-				System.out.println(false);
-				return;
+				return false;
 			}
 			i++;
 			j--;
 		}
-		System.out.println(true);
+		return true;
 	}
 }
--- src/Lecture12/Substring.java
+++ src/Lecture12/Substring.java
@@ -1,16 +1,21 @@
 package Lecture12;
 
-import java.util.Scanner;
+import java.util.*;
 
-import org.w3c.dom.ls.LSOutput;
 
 public class Substring {
 	public static void main(String[] args) {
 		Scanner s= new Scanner(System.in);
 		String s1=s.nextLine();
+		subString(s1);
+	}
+	public static void subString(String s1) {
 		for(int i=0; i<s1.length(); i++) {
 			for(int j=i+1; j<=s1.length(); j++) {
-			System.out.println(s1.substring(i,j));
+				//code to print only palindromF
+				if(PalindromCheck.Palindrom(s1.substring(i,j))) {
+					System.out.println(s1.substring(i,j));
+				}
 			}
 		}
 	}
