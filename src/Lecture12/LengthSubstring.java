13a76e62a32cfc127a95f7cdb38694b16ad016fe
Shubham <shubham.shubham.singh13@gmail.com>
2023-07-14 13:32:26 +0530

    printing in seqquence

---
+++ src/Lecture12/LengthSubstring.java
@@ -0,0 +1,21 @@
+package Lecture12;
+
+import java.util.Scanner;
+
+public class LengthSubstring {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String s1=s.nextLine();
+		PrintSubstring(s1);
+		
+	}
+	public static void PrintSubstring(String S) {
+		for(int i=1; i<=S.length(); i++) {
+			for(int j=i; j<=S.length(); j++) {
+				int len=j-i;
+				System.out.println(S.substring(len,j));
+			}
+		}
+	}
+
+}
