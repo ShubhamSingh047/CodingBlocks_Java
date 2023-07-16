13072b8b252f4af2a73d0659e048f77d4532e76e
Shubham <shubham.shubham.singh13@gmail.com>
2023-07-16 22:00:21 +0530

    equilibrium in arr

--- src/PrefixSum/EquilibriumPoint.java
+++ src/PrefixSum/EquilibriumPoint.java
@@ -2,15 +2,21 @@ package PrefixSum;
 
 public class EquilibriumPoint {
 	public static void main(String[] args) {
-		int[] arr= {3,4,8,-9,20,6};
+		int[] arr= {0,-1,1,6};
 		int n=arr.length;
-		int[] prefix_arr=new int[n];
-		prefix_arr[0]=arr[0];
-		for(int i=1; i<n; i++) {
-			prefix_arr[i]=prefix_arr[i-1]+arr[i];
-		}
 		for(int i=0; i<n; i++) {
-			if()
+			int lsum=0,rsum=0;
+			for(int j=0; j<i; j++) {
+				lsum+=arr[j];
+			}
+			for(int j=i+1; j<n; j++) {
+				rsum+=arr[j];
+			}
+			if(lsum==rsum) {
+				System.out.println(true);
+				return;
+			}
 		}
+		System.out.println(false);
 	}
 }
--- src/Self_Sliding_Window/N_binacci.java
+++ src/Self_Sliding_Window/N_binacci.java
@@ -10,7 +10,7 @@ public class N_binacci {
 		arr[n-1]=1;
 		arr[n]=1;
 		for(int i=n+1; i<m; i++) {
-			arr[i]=2*arr[i-1]+arr[i-n-1];
+			arr[i]=2*arr[i-1]-arr[i-n-1];
 		}
 		for(int i:arr) {
 			System.out.println(i);
