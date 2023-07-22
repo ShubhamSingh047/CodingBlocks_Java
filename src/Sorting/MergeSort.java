da9d00439b9ea951f4998efd1d05d8d1a9ec0451
Shubham <shubham.shubham.singh13@gmail.com>
2023-07-22 21:39:51 +0530

    merge sort

--- src/Sorting/MergeSort.java
+++ src/Sorting/MergeSort.java
@@ -17,36 +17,29 @@ public class MergeSort {
 		Merge(arr,s,mid,e);
 	}
 	public static void Merge(int[] arr,int s,int mid, int e) {
-		int temp[]=new int[s+e+1];
+		System.out.println(s+" "+e);
+		int temp[]=new int[e-s+1];
 		int left=s,right=mid+1,k=0;
 		while(left<=mid && right<=e) {
 			if(arr[left]<arr[right]) {
-				temp[k]=arr[left];
-				left++;
+				temp[k++]=arr[left++];
 			}else if(arr[right]<arr[left]) {
-				temp[k]=arr[right];
-				right++;
+				temp[k++]=arr[right++];
 			}
 			else {
-				temp[k]=arr[left];
-				left++;
+				temp[k++]=arr[left++];
 				right++;
 			}
-			k++;
 		}
 		while(left<=mid) {
-			temp[k]=arr[left];
-			k++;
-			left++;
+			temp[k++]=arr[left++];
 		}
 		while(right<=e) {
-			temp[k]=arr[right];
-			k++;
-			right++;
+			temp[k++]=arr[right++];
 		}
 		//we are taking equal to e as e=n-1 which has been passed from main method;
-		  for (int i = 0; i <= e; i++) {
-	            arr[i] = temp[i];
-	        }
+		for (int i = s; i < temp.length; i++) {
+	           arr[i] = temp[i-s];
+	    }
 	}
 }
