258f74c66ab1334015a763f43e75603cfb4018ea
Shubham <shubham.shubham.singh13@gmail.com>
2023-08-11 14:53:14 +0530

    unique elment

--- src/Searching/UniqueElment.java
+++ src/Searching/UniqueElment.java
@@ -3,19 +3,27 @@ package Searching;
 public class UniqueElment {
 	public static void main(String[] args) {
 		int[] arr= {1,2,2,3,3,4,4,5,5};
+		if(arr[0]!=arr[1]) {
+			System.out.println(arr[0]);
+			return;
+		}
+		if(arr[arr.length-1]!=arr[arr.length-2]) {
+			System.out.println(arr[arr.length-1]);
+			return;
+		}
+
 		int low=1, high=arr.length-2;
+		
 		while(low<=high) {
 			int mid=(low+high)/2;
-			if(arr[mid-1]==arr[mid]||arr[mid+1]==arr[mid]) {
-				int left=mid-low-1;
-				if(left%2==0) {
-					low=mid+1;
-				}else {
-					high=mid-1;
-				}
+			if((arr[mid]!=arr[mid-1])&& arr[mid]!=arr[mid+1]){
+			System.out.println(arr[mid]);
+			return;
+			}
+			if((mid%2==1 && arr[mid+1]==arr[mid])||(arr[mid+1]==arr[mid] && mid%2==0)) {
+				low=mid+1;
 			}else {
-				System.out.println(arr[mid]);
-				return;
+				high=mid-1;
 			}
 		}
 		System.out.println(-1);
