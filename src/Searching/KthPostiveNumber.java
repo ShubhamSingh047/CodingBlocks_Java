a02026cdbc2fcbe1d69a6181d8439093efc582cc
Shubham <shubham.shubham.singh13@gmail.com>
2023-08-14 17:42:08 +0530

    find kth postive number

--- src/Searching/KthPostiveNumber.java
+++ src/Searching/KthPostiveNumber.java
@@ -1,5 +1,27 @@
 package Searching;
 
-public class KthPostiveNumber {
+import java.util.Scanner;
 
+public class KthPostiveNumber {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int k=s.nextInt();
+		int arr[]=new int[n];
+		for(int i=0; i<n; i++) {
+			arr[i]=s.nextInt();
+		}
+		findKthPositive(arr,k);
+	}
+	public static int findKthPositive(int[] arr, int k) {
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]<=k){
+                k++;
+            }else{
+                break;
+            }
+        }
+        return k;
+    }
+	public static int BS_approch_k(a) 
 }
