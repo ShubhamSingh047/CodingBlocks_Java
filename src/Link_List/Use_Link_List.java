66b12d3f5b528b1facdc23ddcd8ad625a31b49ba
Shubham <shubham.shubham.singh13@gmail.com>
2023-08-30 22:42:02 +0530

    link list

---
+++ src/Link_List/Use_Link_List.java
@@ -0,0 +1,9 @@
+package Link_List;
+
+public class Use_Link_List {
+	public static void main(String[] args) {
+		Link_List ll=new Link_List();
+		Link_List.Node nn= ll.new Node();
+		ll.Display();
+	}
+}
