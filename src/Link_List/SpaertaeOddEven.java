7672fd808902e9bab056bb6afba851fe2de7d6d3
Shubham <shubham.shubham.singh13@gmail.com>
2023-09-03 14:38:03 +0530

    Odd Even sort

---
+++ src/Link_List/SpaertaeOddEven.java
@@ -0,0 +1,26 @@
+package Link_List;
+
+public class SpaertaeOddEven {
+    public ListNode oddEvenList(ListNode head) {
+        ListNode temp=head;
+        ListNode odd=new ListNode();
+        ListNode oddH=odd;
+        ListNode Even=new ListNode();
+        ListNode headE=Even;
+        boolean curr=true;
+        while(temp!=null){
+            if(curr){
+                odd.next=temp;
+                odd=odd.next;
+            }else{
+                Even.next=temp;
+                Even=Even.next;
+            }
+            curr=!curr;
+            temp=temp.next;
+        }
+        odd.next=headE.next;
+        Even.next=null;
+        return oddH.next;
+    }
+}
