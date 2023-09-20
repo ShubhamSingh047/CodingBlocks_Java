88e28aa17d31fbcd8accf6ff6880f1cc5c42920e
Shubham <shubham.shubham.singh13@gmail.com>
2023-09-20 12:58:52 +0530

    tree

--- DSA/src/Queque/basicSTL.java
+++ DSA/src/Queque/basicSTL.java
@@ -1,5 +1,12 @@
 package Queque;
 
-public class basicSTL {
+import java.util.*;
 
+public class basicSTL {
+	public static void main(String[] args) {
+		Queue<Integer> q= new LinkedList<>();
+		q.add(1);
+		q.add(2);
+		System.out.println(q);
+	}
 }
