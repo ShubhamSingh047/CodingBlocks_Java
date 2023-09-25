6ba27b07c06cad4122f8a2b78ab7a9f3fe9821e2
Shubham <shubham.shubham.singh13@gmail.com>
2023-09-25 17:24:42 +0530

    bT

--- DSA/src/Tree/BT_to_DL.java
+++ DSA/src/Tree/BT_to_DL.java
@@ -1,5 +1,21 @@
 package Tree;
 
 public class BT_to_DL {
+	static NodeTree prev=null;
+	public static NodeTree Convertion(NodeTree root) {
+		if(root==null) {
+			return root;
+		}
+		NodeTree head=Convertion(root.left);
+		if(prev==null) {
+			head=root;
+		}else {
+			root.left=prev;
+			prev.right=root;
+		}
+		prev=root;
+		Convertion(root.right);
+		return head;
+	}
 
 }
--- DSA/src/Tree/checkBalanceBt.java
+++ DSA/src/Tree/checkBalanceBt.java
@@ -1,5 +1,12 @@
 package Tree;
 
 public class checkBalanceBt {
-
+	public int CheckBalance(NodeTree root) {
+		if(root==null)return 0;
+		int Lt=CheckBalance(root.left);
+		int Rt=CheckBalance(root.right);
+		if(Lt==-1||Rt==-1)return -1;
+		if(Math.abs(Lt-Rt)>1)return -1;
+		return Math.max(Rt,Lt)+1;
+	}
 }
